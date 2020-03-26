f8e6ddbe3d3f3c37e09408f33c7b13fbc258f23a
jtbspace <jtbspace.tech@gmail.comm>
2020-03-26 19:07:41 +1300

    Updates

---
+++ precache-manifest.b5a98aa176b1537cc169a4113d801965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52db96f446c6ae5a5c46dcc991894ac8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d8dc30d4e7ded7c34c9e",
+    "url": "/covid-19/static/css/main.045ab4fe.chunk.css"
+  },
+  {
+    "revision": "3fe97f656807bc19a1e9",
+    "url": "/covid-19/static/js/2.8a17bfaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.8a17bfaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8dc30d4e7ded7c34c9e",
+    "url": "/covid-19/static/js/main.1cd42198.chunk.js"
+  },
+  {
+    "revision": "8a52c8d37d83ffe3717e",
+    "url": "/covid-19/static/js/runtime-main.1a3018c3.js"
+  }
+]);
